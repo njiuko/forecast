4d550fd9ca4638195e17f0aecd51db8b2f4617b7
unnu <norman.timmler@gmail.com>
2016-11-11 17:06:50 +0100

    Implemented half_day? method

--- lib/forecast/assignment.rb
+++ lib/forecast/assignment.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 module Forecast
   class Assignment < Base
 
@@ -7,6 +9,20 @@ module Forecast
 
     def person
       Person.all.find { |person| person_id == person.id }
+    end
+
+    def half_day?
+      return unless allocation
+
+      start_date == end_date && allocation <= 4.hours
+    end
+
+    def start_date
+      Date.parse(@attributes[:start_date])
+    end
+
+    def end_date
+      Date.parse(@attributes[:end_date])
     end
   end
 end
